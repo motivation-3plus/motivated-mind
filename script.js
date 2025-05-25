830410d4b7332d8ad623748f680ebb753201e665
motivation-3plus <coffee.jtd@gmail.com>
2025-05-26 01:14:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// You can add animations or click effects here if needed
-console.log("Welcome to Motivated Mind!");
+function speak(message) {
+  const synth = window.speechSynthesis;
+  const utter = new SpeechSynthesisUtterance(message);
+  utter.rate = 1;
+  utter.pitch = 1;
+  utter.lang = 'en-US';
+  synth.speak(utter);
+}
+
+console.log("Interactive Motivated Mind Dashboard Loaded");
